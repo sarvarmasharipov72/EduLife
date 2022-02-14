4e855febcbced71248f23cdbb2eaf09609c2fb33
Sarvar Masharipov <sarvarmasharipov72>
2022-02-14 16:06:48 +0300

    changes

---
+++ app/src/main/java/sarvarmasharipov72/edulife/ui/Courses/model/MyCourse.kt
@@ -0,0 +1,11 @@
+package sarvarmasharipov72.edulife.ui.Courses.model
+
+data class MyCourse(
+    val nameMyCourse: String,
+    val video: Int,
+    val complateVideo: Int,
+    val task: Int,
+    val complateTask: Int,
+    val test: Int,
+    val complateTest: Int
+)
---
+++ app/src/main/java/sarvarmasharipov72/edulife/ui/Courses/model/Recommended.kt
@@ -0,0 +1,9 @@
+package sarvarmasharipov72.edulife.ui.Courses.model
+
+data class Recommended(
+    val levelCourse: String,
+    val nameCourse: String,
+    val textCourse: String,
+    val price: Int,
+    val discount: Int
+)
